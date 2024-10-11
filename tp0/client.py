62cc5692d43bafdbd882e09c9883c1c14c450cae
EzequielOrlanes <you@example.com>
2024-10-11 19:25:00 -0300

    maybe finished

--- tp0/client.py
+++ tp0/client.py
@@ -7,8 +7,6 @@ def individual_token_request(server_address, student_id, nonce):
     student_id = student_id.ljust(12)
     # Tipo de mensagem: 2 bytes, valor 1 (para o request)
     message_type = 1
-    # Monta a mensagem (18 bytes: tipo, ID, nonce)
-    # Formato de struct: H para um unsigned short (2 bytes), 12s para string de 12 bytes, I para unsigned int (4 bytes)
     request_format = ">H12sI"
     message = struct.pack(
         request_format, message_type, bytes(student_id, encoding="ascii"), nonce
@@ -47,8 +45,6 @@ def individual_token_validation(server_address, student_id, nonce, token):
     student_id = student_id.ljust(12)
     # Tipo de mensagem: 2 bytes, valor 1 (para o request)
     message_type = 3
-    # Monta a mensagem (18 bytes: tipo, ID, nonce)
-    # encode token para ser estar bytes
     token_e = token.encode("utf-8")
     # Formato de struct: H para um unsigned short (2 bytes), 12s para string de 12 bytes, I para unsigned int (4 bytes), 64s para 64 bytes.
     request_format = ">H12sI64s"
@@ -68,7 +64,6 @@ def individual_token_validation(server_address, student_id, nonce, token):
                 response, _ = sock.recvfrom(1024)
                 return response
         except socket.error as e:
-            print(f"Erro ao conectar-se usando {family}: {e}")
             continue
 
 
@@ -91,9 +86,6 @@ def individual_token_status(response):
     student_id = student_id.decode("ascii").strip()
     # Decodifica o token (a string hexadecimal de 64 bytes)
     token = token.decode("ascii")
-    print("Individual token status: ")
-    print(message_type, student_id, nonce, token, status)
-    print("________________________________________")
     return message_type, student_id, nonce, token, status
 
 
@@ -120,23 +112,16 @@ def group_token_response(response, num_sas):
     response_format = f">HH{80 * num_sas}s64s"
     # Desempacota a resposta conforme o formato correto
     message_type, num_sas_, sas_bytes, token = struct.unpack(response_format, response)
-    # sas = sas.decode("ascii")
-    # Exibe os resultados
-    # Decodifica o token (a string hexadecimal de 64 bytes)
     token = token.decode("ascii")
-    print("*************1**************")
-    print(message_type, num_sas_, sas_bytes, token)
-    print("**************1*************")
-    tamanho_em_bytes = len(response)
-    print("O tamanho da resposta group token request em bytes é:", tamanho_em_bytes)
     return message_type, num_sas_, sas_bytes, token
 
 
-def group_token_validation(server_address, message_type, num_sas_, sas_bytes, token):
+def group_token_validation(server_address, message_type, num_sas, sas_bytes, token):
     message_type = 7
-    request_format = f">HH{80 * num_sas_}s64s"
-    token = token.encode("utf-8")
-    message = struct.pack(request_format, message_type, num_sas_, sas_bytes, token)
+    request_format = f">HH{80 * num_sas}s64s"
+    token_e = token.encode("utf-8")
+    # sas_bytes = sas_bytes.encode("utf-8")
+    message = struct.pack(request_format, message_type, num_sas, sas_bytes, token_e)
     # Cria o socket UDP e envia a mensagem para o servidor
     families = [socket.AF_INET6, socket.AF_INET]
     for family in families:
@@ -151,16 +136,12 @@ def group_token_validation(server_address, message_type, num_sas_, sas_bytes, to
 
 
 def group_token_status(response, num_sas):
-    response_format = f">HH{80 * num_sas}s64s1s"
-    message_type, num_sas_, sas_bytes, token, s = struct.unpack(
+    response_format = f">HH{80 * num_sas}s64sB"
+    message_type, num_sas_, sas_bytes, token, status = struct.unpack(
         response_format, response
     )
     token = token.decode("ascii")
-    print("***************************")
-    print(message_type, num_sas_, sas_bytes, token, s)
-    print("***************************")
-    tamanho_em_bytes = len(response)
-    print("O tamanho da resposta group token request em bytes é:", tamanho_em_bytes)
+    return message_type, num_sas_, sas_bytes, token, status
 
 
 def convert_sas_to_bytes(sas_list):
@@ -185,46 +166,61 @@ def convert_sas_to_bytes(sas_list):
 
 def main():
     while True:
-        command_line = input(
-            "Digite o comando (e.g., itr <id> <nonce>, ou 'sair' para encerrar):"
-        )
+        command_line = input("")
         parts = command_line.split(" ")
+        global student_id, nonce, token
         if command_line and len(parts) >= 3:
             host = parts[0]
-            port = int(parts[1])  # Converter porta para inteiro
+            port = int(parts[1])
             command = parts[2]
-            args = parts[-1]
-            args_ = args.split(" ")
+            args = parts[3:]
             if command == "itr":
                 message_type, student_id, nonce, token = individual_token_response(
-                    individual_token_request((host, port), str(args_[0]), args_[1])
+                    individual_token_request((host, port), str(args[0]), int(args[1]))
                 )
-            print("individual token response:")
-            print(message_type, student_id, nonce, token)
-        # elif command == "gtr":
+                print(message_type, student_id, nonce, token)
 
-    # individual_token_status(
-    #     individual_token_validation(
-    #         ("150.164.213.243", 51001), student_id, nonce, token
-    #     )
-    # )
-    # sas_list = []
-    # sas1 = {"student_id": student_id, "nonce": nonce, "token": token}
-    # sas_list.append(sas1)
-    # sas2 = {"student_id": student_id, "nonce": nonce, "token": token}
-    # sas_list.append(sas2)
-    # print(sas_list)
+            if command == "itv":
+                message_type, student_id, nonce, token, status = (
+                    individual_token_status(
+                        individual_token_validation(
+                            ("150.164.213.243", 51001), student_id, nonce, token
+                        )
+                    )
+                )
+                print(message_type, student_id, nonce, token, status)
 
-    # message_type_g, num_sas_g, sas_bytes_g, token_g = group_token_response(
-    #     group_token_request(("150.164.213.243", 51001), convert_sas_to_bytes(sas_list)),
-    #     len(sas_list),
-    # )
-    # group_token_status(
-    #     group_token_validation(
-    #         ("150.164.213.243", 51001), message_type_g, num_sas_g, sas_bytes_g, token_g
-    #     ),
-    #     len(sas_list),
-    # )
+            if command == "gtr":
+                sas_list = []
+                for token_args in args:
+                    sas = {
+                        "student_id": student_id,
+                        "nonce": nonce,
+                        "token": token_args,
+                    }
+                    sas_list.append(sas)
+                message_type_g, num_sas_g, sas_bytes_g, token_g = group_token_response(
+                    group_token_request(
+                        ("150.164.213.243", 51001), convert_sas_to_bytes(sas_list)
+                    ),
+                    len(sas_list),
+                )
+                sas_bytes_gb = sas_bytes_g.decode("utf-8", errors="ignore")
+                print(message_type_g, num_sas_g, sas_bytes_gb, token_g)
+
+            if command == "gtv":
+                message_type, num_sas_, sas_bytes, token, s = group_token_status(
+                    group_token_validation(
+                        ("150.164.213.243", 51001),
+                        message_type_g,
+                        num_sas_g,
+                        sas_bytes_g,
+                        token_g,
+                    ),
+                    len(sas_list),
+                )
+                sas_bytes = sas_bytes.decode("utf-8", errors="ignore")
+                print(message_type, num_sas_, sas_bytes, token, s)
 
 
 if __name__ == "__main__":
